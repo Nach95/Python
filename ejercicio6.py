3668925cde7f9ec1b04d06b4ac73c1f95f88e8d4
Nach95 <43922116+Nach95@users.noreply.github.com>
2018-12-03 12:35:43 -0600

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -5,7 +5,7 @@
 #Leal Gonzalez Ignacio Ejercicio 6 Diccionario por compresion
 
 '''
-Se va a realizar un diccionario por compresion en el cula las llaves son los números odiosos menores a 50 y el valor es una tupla de dos elementos: su representación en binario y su representación en
+Se va a realizar un diccionario por compresion en el cual las llaves son los números odiosos menores a 50 y el valor es una tupla de dos elementos: su representación en binario y su representación en
 hexadecimal, un número odioso (odious number) es todo aquél que su representación en binario tiene un número impar de unos, por lo tanto haremos un ciclo for que empiece en uno y termine en 49, a
 cada numero se convertira a binario y se contara cuantos unos tiene y se le hara el modulo si da un resultado diferente a cero es un numero odioso y se guarda en el diccionario en caso contrario
 no se guardara.
